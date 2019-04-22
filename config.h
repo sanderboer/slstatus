9bb9c393c988703965b3d783ce2e4ab7d44f1600
amariampolskiy <amariampolskiy@gmail.com>
2019-04-22 18:48:21 +0300

    prettier status bars

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-const unsigned int interval = 1000;
+const unsigned int interval = 500;
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
@@ -63,6 +63,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
-	{ cpu_perc, ";CPU: %s%%",    "" },
+	{ run_command, " 📅 %s ",    "date +'%a %b %d'"},
+	{ run_command, "⏰ %s",      "date +%T"},
+	{ cpu_perc,    ";💻 %s%%",   "" },
+	{ temp,        " 🌡 %s",      "/sys/class/thermal/thermal_zone0/temp" },
+	{ temp,        "/%s C°",     "/sys/class/thermal/thermal_zone1/temp" },
 };
